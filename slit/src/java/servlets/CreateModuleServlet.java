db80187255f4666675851d5f81cf8f4c6a515b8e
Martin <martsk16@student.uia.no>
2017-10-19 15:45:27 +0200

    Added RandomStringGenerator for the reset pass function

--- slit/src/java/servlets/CreateModuleServlet.java
+++ slit/src/java/servlets/CreateModuleServlet.java
@@ -62,6 +62,7 @@ public class CreateModuleServlet extends HttpServlet {
                 ps.setString(5, newResource);
                 ps.setString(6, newTask);
                 ps.setDate(7, Date.valueOf(stringDeadline));
+                ps.executeQuery();
                 createdModule = true;
             }
         } catch(SQLException e){
