05e8db0226d83168a5eeeac7ebe5a47269cd8e0f
mariusvika <mariuv16@student.uia.no>
2017-10-29 17:18:11 +0100

    Added test data for slitdb.sql

--- slitdb.sql
+++ slitdb.sql
@@ -42,4 +42,32 @@ create table handIn(
 	constraint user_fk foreign key (u_id) references userAccount (id)
 );
 
+
 drop table handIn;
+
+/*TEST DATA*/
+INSERT INTO userAccount (id, firstName, lastName, email, pass, role, lastLogin, resetVerification) 
+VALUES (id, 'Marius', 'Vika', 'mariuv16@uia.no', 'hei123', '0', null, null);
+
+INSERT INTO userAccount(id, firstName, lastName, email, pass, role, lastLogin, resetVerification) 
+VALUES (id, 'Hallgeir', 'Nilsen', 'halgeir@nilsen.no', 'halnils', '1', null, null);
+
+INSERT INTO userAccount(id, firstName, lastName, email, pass, role, lastLogin, resetVerification)
+VALUES (id, 'Even', 'Larsen', 'even@larsen.no', 'evlar', '1', null, null);
+
+INSERT INTO userAccount(id, firstName, lastName, email, pass, role, lastLogin, resetVerification)
+VALUES (id, 'Anders', 'Ekse', 'anders.xe@gmail.com', 'testpassword', '0', null, null);
+
+
+INSERT INTO module (id, title, description, goals, resources, task, deadline) 
+VALUES ('1', 'Modul 1', 'Lag en brusautomat i blueJ.', 'lære basic kode i java', 'Object oriented coding - lærerbok', 'Lag en brusautomat med bruk av en klasse, skriv også ned en logg etter fullført.', '2017-11-10');
+
+INSERT INTO module (id, title, description, goals, resources, task, deadline) 
+VALUES ('2', 'Modul 2', 'Lag en sykkelbutikk i java.', 'lære mer avansert kode i java', 'Object oriented coding - lærerbok', 'Lag en sykkelbutikk ved bruk av minst tre klasser. Skriv også en læringslogg.', '2017-12-10');
+
+
+INSERT INTO handIn (fileName, fileBlob, deliveryDate, feedback, notes, points, m_id, u_id) 
+VALUES ('MariusSinModul1', null, deliveryDate, 'Veldig bra jobbet!!', 'Dette er min modul 1, håper den er bra!', '10', '1', '1');
+
+INSERT INTO handIn (fileName, fileBlob, deliveryDate, feedback, notes, points, m_id, u_id) 
+VALUES ('Modul2Anders', null, deliveryDate, 'Helt OK, dette må du jobbe litt mer med', 'Min modul 2, håper dere liker.', '5', '2', '4');
